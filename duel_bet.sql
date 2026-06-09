d36eac3fc93e4a5799b1112dc320e25dacdcb064
Zyggy123 <57183441+zyggy123@users.noreply.github.com>
2026-06-09 19:42:46 +0200

    Add indexes for player GUIDs in duel_bet table

--- duel_bet.sql
+++ duel_bet.sql
@@ -4,7 +4,9 @@ CREATE TABLE IF NOT EXISTS `duel_bets` (
   `player2_guid` INT UNSIGNED NOT NULL,
   `amount` BIGINT UNSIGNED NOT NULL,
   `status` TINYINT UNSIGNED DEFAULT 0 COMMENT '0: Pending, 1: Active, 2: In-Duel',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  INDEX `idx_p1` (`player1_guid`),
+  INDEX `idx_p2` (`player2_guid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `duel_bet_stats` (
