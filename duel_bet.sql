61e10f08b5960123289c23764ac3925d9fff3a7a
Zyggy123 <57183441+zyggy123@users.noreply.github.com>
2026-06-09 18:14:39 +0200

    Create duel_bets and duel_bet_stats tables

---
+++ duel_bet.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS `duel_bets` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `player1_guid` INT UNSIGNED NOT NULL,
+  `player2_guid` INT UNSIGNED NOT NULL,
+  `amount` BIGINT UNSIGNED NOT NULL,
+  `status` TINYINT UNSIGNED DEFAULT 0 COMMENT '0: Pending, 1: Active, 2: In-Duel',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `duel_bet_stats` (
+  `guid` INT UNSIGNED NOT NULL,
+  `total_duels` INT UNSIGNED DEFAULT 0,
+  `total_won` INT UNSIGNED DEFAULT 0,
+  `total_profit` BIGINT DEFAULT 0,
+  PRIMARY KEY (`guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
